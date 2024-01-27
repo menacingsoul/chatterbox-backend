205890f045e2eadf983aa9fe0c4a6824ea40625a
menacingsoul <adarsh14103@gmail.com>
2024-01-27 12:05:03 +0530

    .

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ mongoose
   .then(() => {
     console.log("MongoDB connected");
     // Start the server after MongoDB connection is established
-    app.listen(PORT, () => {
+    app.listen(PORT,"0.0.0.0", () => {
       console.log(`Server is running on port ${PORT}`);
     });
   })
